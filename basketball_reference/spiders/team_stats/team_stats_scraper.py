0eef27aad03a57c7450b11e32725c7219ce13f50
Jon Liauw <jliauw@sonobi.com>
2019-01-08 10:49:42 -0500

    Add Team/Opponent stats

--- basketball_reference/spiders/team_stats/team_stats_scraper.py
+++ basketball_reference/spiders/team_stats/team_stats_scraper.py
@@ -17,19 +17,18 @@ def main():
     # "opponent-stats-per_game",
     "team-stats-base",
     "opponent-stats-base",
-    "team-stats-per_poss",
-    "opponent-stats-per_poss",
-    "misc_stats",
-    "team_shooting",
-    "opponent_shooting",
+    # "team-stats-per_poss",
+    # "opponent-stats-per_poss",
+    # "misc_stats",
+    # "team_shooting",
+    # "opponent_shooting",
   ]
 
   for table_id in table_ids:
-    # response_html = response.html.find("#{}".format(table_id), first=True)
-    f = open("{}.html".format(table_id), "r")
-    # html = f.write(response_html.html)
-    html = f.read()
-    soup = BeautifulSoup(html, 'html.parser')
+    # response_html = response.html.find("#{}".format(table_id), first=True).html
+    f = open("{}.html".format(table_id), "r")   # DEV
+    response_html = f.read()                    # DEV
+    soup = BeautifulSoup(response_html, 'html.parser')
     
     if table_id in ["confs_standings_E", "confs_standings_W"]:
       teams = soup.find("table", {"id": "{}".format(table_id)}).find("tbody").findAll("tr")
@@ -42,12 +41,12 @@ def main():
         seed = int(seed[seed.find("(") + 1 : seed.find(")")])
         standings[conference][team_name] = {"seed": seed}
 
-        tds = team.findAll("td")
-        for td in tds:
-          data_stat = td["data-stat"]
+        fields = team.findAll("td")
+        for field in fields:
+          data_stat = field["data-stat"]
 
           # convert to proper type
-          val = td.text
+          val = field.text
           if "." in val:
             val = float(val)
           elif val == "—":
@@ -56,25 +55,44 @@ def main():
             val = int(val)
 
           standings[conference][team_name][data_stat] = val
+
+      # for standing in standings:
+      #   for team in standings[standing]:
+      #     print(team, standings[standing][team])
+
+      # TODO: Store in DB
+
+    elif table_id in ["team-stats-base", "opponent-stats-base"]:
+      teams = soup.find("table", {"id": "{}".format(table_id)}).find("tbody").findAll("tr")
+      to = "team" if table_id in "team-stats-base" else "opponent"
+      team_stats = { to: {}}
+      for team in teams:
+        team_name = team.find("td", {"data-stat": "team_name"}).a["href"].split("/")[2]
+        rank  = int(team.find("th", {"data-stat" : "ranker"}).text)
+        team_stats[to][team_name] = {"rank" : rank }
+        
+        fields = team.findAll("td")
+        for field in fields[1:]:
+          team_stats[to][team_name][field["data-stat"]] = float(field.text) if "." in field.text else int(field.text)
+
+      # for to in team_stats:
+      #   for team in team_stats[to]:
+      #     print(team, team_stats[to][team])
+
+      # TODO: Store in DB
+
+    elif table_id == "team-stats-per_poss": # in [, "opponent-stats-per_poss"]
       
-      for standing in standings:
-        for team in standings[standing]:
-          print(team, standings[standing][team])
 
-
-      # print(east_standings)
-    # elif table_id == :
-    # elif table_id == "team-stats-base":
-    # elif table_id == "opponent-stats-base":
-    # elif table_id == "team-stats-per_poss":
-    # elif table_id == "opponent-stats-per_poss":
     # elif table_id == "misc_stats":
-    # elif table_id == "team_shooting":
-    # elif table_id == "opponent_shooting":
+    # elif table_id == "team_shooting": # in [, "opponent_shooting"]
 
 
-
-    f.close()
+"""
+scraping
+controller (2)
+message deb/mark
+"""
 
 if __name__ == "__main__":
   main()
