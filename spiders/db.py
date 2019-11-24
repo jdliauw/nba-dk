a8422e25e29273490028a8343753b2f26b69e9a9
jon <jdliauw@gmail.com>
2019-11-24 16:36:17 -0500

    Fix spiders

--- spiders/db.py
+++ spiders/db.py
@@ -17,23 +17,23 @@ def exec(query):
             # logger.exception("Unable to execute query: {0}".format(query))
             conn.rollback()
 
-def delete_pids(self, table):
+def delete_pids(table):
     f = open("db.log", "a")
     for pid in pids:
         try:
             query = "DELETE FROM {} where pid = '{}'".format(table, pid)
-            self.cursor.execute(query)
+            cursor.execute(query)
             f.write("Successfully deleted {} from {}".format(pid, table))
-            self.conn.commit()
+            conn.commit()
         except Exception as e:
-            self.conn.rollback()
+            conn.rollback()
             f.write(str(e))
     f.close()
 
-def insert_to_db(self, insert_query):
+def insert_to_db(insert_query):
     try:
-        self.cursor.execute(insert_query)
-        self.conn.commit()
+        cursor.execute(insert_query)
+        conn.commit()
     except psycopg2.IntegrityError as e:
         pgcode = e.pgcode
         f = open("db.log", "a")
@@ -41,80 +41,15 @@ def insert_to_db(self, insert_query):
             f.write("postgres_error {}: insertion skipped (duplicate)\n".format(e.pgcode))
         else:
             f.write("postgres_error:{},insert_query={}\n".format(e.pgcode, insert_query))
-        self.conn.rollback()
+        conn.rollback()
         f.close()
     except Exception as e:
         f = open("db.log", "a")
         f.write("postgres_error:{},insert_query={}\n".format(e, insert_query))
-        self.conn.rollback()
+        conn.rollback()
         f.close()
 
-def insert(self, stat, table):
-    if table in ["team_shooting_stats", "team_stats", "team_stats_per_100"]:
-        self.insert_nested_dict(stat, table)
-    elif table in ["misc_stats", "standings", "pbp"]:
-        self.insert_list(stat, table)
-    elif table == "player_info":
-
-        # this needs to be first as it the child tables have pid as a foreign key
-        if "player_info" in stat:
-            self.insert_dict(stat["player_info"], "player_info")
-        # nested: stat = {"player_shooting_stats" : [], "player_info": {}...}
-        for table_name in ["player_shooting_stats", "college_stats", "salaries"]:
-            if table_name in stat:
-                self.insert_list(stat[table_name], table_name)
-        # if "player_info" in stat:
-        #     self.insert_dict(stat["player_info"], "player_info")
-        if "contracts" in stat and "pid" in stat["contracts"] and "team" in stat["contracts"]:
-            columns, values = "", ""
-            for key in stat["contracts"].keys():
-                if key not in ["pid", "team"]:
-                    value = stat["contracts"][key]
-                    columns += "{},".format(key)
-                    if isinstance(value, str):
-                        values += "'{}',".format(value)
-                    else:
-                        values += "{},".format(value)
-            columns = columns[:-1]
-            values = values[:-1]
-            query = "INSERT INTO {}({}) VALUES ({});".format("contracts", columns, values)
-            self.insert_dict(stat["contracts"], "contracts") # here
-    elif table == "games":
-        self.insert_list(stat, table)
-    else:
-        values += "{},".format(value)
-    columns = columns[:-1]
-    values = values[:-1]
-    query = "INSERT INTO {}({}) VALUES ({});".format(table, columns, values)
-    exec(query)
-
-def insert_dict(self, dict, table):
-    columns = ""
-    values = ""
-    for key in dict:
-        columns += "{},".format(key)
-        value = dict[key]
-        if isinstance(value, str):
-            if key in ["collected_date", "game_date"]:
-                values += "TO_DATE('{}','YYYYMMDD'),".format(value)
-            else:
-                values += "'{}',".format(value)
-        else:
-            values += "{},".format(value)
-    columns = columns[:-1]
-    values = values[:-1]
-    query = "INSERT INTO {}({}) VALUES ({});".format(table, columns, values)
-    self.insert_to_db(query)
-
-def insert_list(self, list, table):
-    for dict in list:
-        self.insert_dict(dict, table)
-
-def insert_nested_dict(self, nested_dict, table):
-    for index in nested_dict:
-        self.insert_dict(nested_dict[index], table)
-
-def insert(self, stat, table):
+def insert(stat, table):
     if table in ["team_shooting_stats", "team_stats", "team_stats_per_100"]:
         insert_nested_dict(stat, table)
     elif table in ["miscellaneous_stats", "standings", "pbp"]:
@@ -144,6 +79,33 @@ def insert(self, stat, table):
         insert_list(stat, table)
     else:
         print("Error reading {} file".format(stat))
+
+def insert_dict(dict, table):
+    columns = ""
+    values = ""
+    for key in dict:
+        columns += "{},".format(key)
+        value = dict[key]
+        if isinstance(value, str):
+            if key in ["collected_date", "game_date"]:
+                values += "TO_DATE('{}','YYYYMMDD'),".format(value)
+            else:
+                values += "'{}',".format(value)
+        else:
+            values += "{},".format(value)
+    columns = columns[:-1]
+    values = values[:-1]
+    query = "INSERT INTO {}({}) VALUES ({});".format(table, columns, values)
+    insert_to_db(query)
+
+def insert_list(list, table):
+    for dict in list:
+        insert_dict(dict, table)
+
+def insert_nested_dict(nested_dict, table):
+    for index in nested_dict:
+        insert_dict(nested_dict[index], table)
+
 
 """
     def insert_jsons(self, stat):
