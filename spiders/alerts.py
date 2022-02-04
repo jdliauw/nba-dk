f77143d5a6136f35a3fed93299b0e4022ee2f84f
jon <jdliauw@gmail.com>
2022-02-04 14:43:43 -0700

    - Add argparse - Add logging - Fix check games logic

--- spiders/alerts.py
+++ spiders/alerts.py
@@ -1,63 +1,69 @@
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+from datetime import datetime
 from twilio.rest import Client
 
+import argparse
 import constants
+import logging
 import records
 import scraper
-import time
+
 
 class Alert:
-    def __init__(self):
+    def __init__(self, test_mode):
         self.dk_spreads = []
         self.differential = 20
         self.text_entries = {}
         self.sleep_time = 0
+        self.test_mode = test_mode
         self.next_live_game_check = datetime(2022, 1, 1, 0, 0, 0, 0)
 
+    """
+    Check for live games. At the completion of each scenario consider whether you need to:
+    (1) update the next_live_game_check
+    (2) update the sleep time
+    (3) log the scenario
+    """
     def check_for_live_games(self):
+        # scenario 1: we've already checked or are in test mode
         now = datetime.now()
-        if now < self.next_live_game_check:
+        if now < self.next_live_game_check or self.test_mode:
             return
 
+        # scenario 2: there are games that are live, don't re-check until tomorrow at 8 AM
         nba_game_times_url = "https://www.google.com/search?q=nba+games+tonight&rlz=1C5CHFA_enUS873US873&oq=nba+games+tonight&aqs=chrome.0.69i59.2874j0j4&sourceid=chrome&ie=UTF-8"
         soup = scraper.get_soup(nba_game_times_url)
-        try:
-            # Scenario 1: There are games that are live, don't re-check until tomorrow at 8 AM
-            spans = soup.findAll('span', {'class':'imso-medium-font'})
-            if len(spans) > 0:
-                self.sleep_time = 0
-                self.next_live_game_check = datetime(now.year, now.month, now.day+1, 8, 0, 0)
-                print("There are live games, don't re-check for live games until tomorrow at 8 AM")
-                return
+        spans = soup.findAll('span', {'class':'imso-medium-font'})
+        if len(spans) > 0:
+            self.sleep_time = 0
+            self.next_live_game_check = datetime(now.year, now.month, now.day+1, 8, 0, 0)
+            logging.info('There are live games, don\'t re-check for live games until tomorrow at 8 AM')
+            return
 
-            today = soup.find('div', {'class':'imspo_mt__pm-inf imspo_mt__pm-infc imspo_mt__date imso-medium-font'})
-            if today is not None:
-                today = today.text.strip().lower() == 'today'
-            now = datetime.now()
-
-            # Scenario 2: There are no games today. Check tomorrow at 8 AM
-            if not today:
-                self.next_live_game_check = datetime(now.year, now.month, now.day+1, 8, 0, 0)
-                self.sleep_time = (self.next_live_game_check - now).seconds
-                print("There are no games today. Check tomorrow at 8 AM")
-                return
-
-            # Scenario 3: There are games today but they aren't being played yet. Sleep until 10 minutes before the first game start time
-            first_game_time = soup.find('div', {'class':'imspo_mt__ndl-p imspo_mt__pm-inf imspo_mt__pm-infc imso-medium-font'}).text.strip()
+        # scenario 3: there are games today but they aren't being played yet. Sleep until 10 minutes before the first game start time
+        first_game_time = soup.find('div', {'class':'imspo_mt__ndl-p imspo_mt__pm-inf imspo_mt__pm-infc imso-medium-font'})
+        if first_game_time is not None:
+            first_game_time = first_game_time.text.strip()
             if len(first_game_time) > 0:
                 first_game_time = datetime.strptime(first_game_time, '%I:%M %p')
                 first_game_time = datetime(now.year, now.month, now.day, first_game_time.hour, first_game_time.minute, 0)
                 # sleep until ten minutes before the next game time
                 self.sleep_time = (first_game_time - now).seconds - 600
-                print("There are games today but they aren't being played yet. Sleep until 10 minutes before the first game start time")
+                logging.info('There are games today but they aren\'t being played yet. Sleep until 10 minutes before the first game start time')
                 return
 
-        except:
-            print("Failed to check for live games")
+        # scenario 4: there are no live games today or games scheduled for later today. Check tomorrow at 8 AM
+        today = soup.find('div', {'class':'imspo_mt__pm-inf imspo_mt__pm-infc imspo_mt__date imso-medium-font'})
+        if today is not None:
+            today = today.text.strip().lower() == 'today'
+        if not today:
+            self.next_live_game_check = datetime(now.year, now.month, now.day+1, 8, 0, 0)
+            self.sleep_time = (self.next_live_game_check - now).seconds
+            logging.info('There are no games today. Check tomorrow at 8 AM')
+            return
 
-    def get_dk_spreads(self, test_mode):
-        if test_mode:
+    def get_dk_spreads(self):
+        if self.test_mode:
             f = open("htmls/dk_with_scores.html", "r")
             soup = f.read()
             f.close()
@@ -72,7 +78,7 @@ class Alert:
         tbody = soup.find('tbody', {'class': 'sportsbook-table__body'})
         trs = tbody.findAll('tr')
         if len(trs) % 2 != 0:
-            print("ERROR")
+            logging.warning('Found an uneven amount of rows while parsing DraftKings for spreads')
 
         class_team = {'class': 'event-cell__name-text'}
         class_spread = {'class':'sportsbook-outcome-cell__line'}
@@ -87,6 +93,7 @@ class Alert:
             if len(raw_times) > 0 or len(raw_quarters) > 0:
                 self.sleep_time = 10
             # there are no live games, just return and wait for next sleep cycle
+            logging.warning('raw_times != raw_quarters, returning with {}s sleep'.format(self.sleep_time))
             return
 
         times = []
@@ -125,7 +132,8 @@ class Alert:
                     'time_left': times[i],
                 }
                 self.dk_spreads.append(game_spreads)
-            except:
+            except Exception as e:
+                logging.warning('exception while trying to parse rows: {}'.format(e))
                 self.sleep_time = 10
         return
 
@@ -184,7 +192,7 @@ class Alert:
                 if history[team_down]['games'] > 0:
                     differential_text = "{0}/{1}".format(history[team_down]['wins'], history[team_down]['games'])
 
-                text = """The {} ({}, {}) are down {} {} the {} ({}, {}) in the {} {}\n\nComebacks (down {} or more): {}.\n\nDK ODDS\nML: {}\nSpread: {} at {}""".format(
+                text = """The {} ({}, {}) are down {} {} the {} ({}, {}) in the {} ({} left)\n\nComebacks (down {} or more): {}.\n\nDK ODDS\nML: {}\nSpread: {} at {}""".format(
                     dk_spread['{}_team'.format(team_down)].split(" ")[1],
                     history[team_down]['last_10'],
                     history[team_down]['overall'],
@@ -203,33 +211,34 @@ class Alert:
                 )
 
                 key = dk_spread['home_team']
-                # We've already sent a text
+                # we've already sent a text
                 if key in self.text_entries:
                     continue
                 else:
                     self.text_entries[key] = text
         
-        print(diffs)
+        logging.info('Game score diffs: {}'.format(diffs))
         return
 
-    def send_texts(self, test_mode):
+    def send_texts(self):
+        now = datetime.now()
         if len(self.text_entries) == 0:
-            print("No games satisfy differential condition")
+            logging.info('No games satisfy differential condition')
         for text_entry in self.text_entries:
             text = self.text_entries[text_entry]
             if len(text) > 0:
                 # preserve the key in the dict but empty the value, so we won't send this again
                 self.text_entries[text_entry] = ""
 
-                if test_mode:
-                    print("-----")
+                if self.test_mode:
                     print(text)
-                    print("-----")
                 else:
                     for phone_number in constants.phone_numbers:
+                        if phone_number != constants.phone_numbers[0] and now.hour > 20 and now.minute > 30:
+                            continue
                         client = Client(constants.twilio_sid, constants.twilio_token)
                         message = client.messages.create(body=text, from_=constants.twilio_number, to=phone_number)
-                        print("Message '{0}' sent to {1}.".format(message.body, phone_number))
+                        logging.info("Message '{0}' sent to {1}.".format(message.body, phone_number))
 
     def check_for_inactive_players(self):
         # https://sports.yahoo.com/nba/charlotte-hornets-boston-celtics-2022020202/
@@ -240,21 +249,31 @@ class Alert:
         scraper.sleep(self.sleep_time-ten_percent, self.sleep_time+ten_percent)
 
 
-def main():
-    historic = records.HistoricRecords(False)
-    alert = Alert()
+if __name__ == '__main__':
+
+    # parse if we're in test mode or not
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--test-mode', type=bool, default=True, help='Are we running in test mode?')
+    args = parser.parse_args()
+
+    # set up logging
+    logging.basicConfig(filename='logs/alert.log', level=logging.DEBUG)
+
+    historic = records.HistoricRecords(args.test_mode)
+    alert = Alert(args.test_mode)
+
     while True:
+        now = datetime.now()
+        logging.info('Iteration at {}:{}:{}'.format(now.hour, now.minute, now.second))
         alert.check_for_live_games()
         if alert.sleep_time == 0:
-            alert.get_dk_spreads(False)
+            alert.get_dk_spreads()
         if alert.sleep_time > 0:
             alert.sleep()
             continue
         historic.get_records()
         alert.check_spreads(historic.Records)
-        alert.send_texts(False)
+        alert.send_texts()
 
-        # Run every 1.5 - 2.5 minutes
+        # run every 1.5 - 2.5 minutes
         scraper.sleep(120, 150)
-
-main()
